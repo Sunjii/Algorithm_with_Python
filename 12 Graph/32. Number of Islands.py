f5ce7f9d1b864616ab12e8b9f75526f5e1e6bfb3
Damas J Lee <sophieadenn@gmail.com>
2021-09-11 01:29:28 +0900

    #32 Number of Insland

--- 12 Graph/32. Number of Islands.py
+++ 12 Graph/32. Number of Islands.py
@@ -17,47 +17,33 @@ output : 1
 
 from typing import List
 
+class Solution:
+    def numIslands(grid:List[List[str]]):
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or \
+                j < 0 or j >= len(grid[0]) or \
+                    grid[i][j] != '1':
+                    return
 
-def solutions(grid:List[List[str]]):
-    # 예외 처리
-    if not grid:
-        return 0
-    count = 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == '1':
-                dfs(grid, i, j)
-                count += 1
+            grid[i][j] = 0
+            # 재귀
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
     
-    return count
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        
+        return count
 
+    input = ['11110',
+    '11010',
+    '11000',
+    '00000']
 
-def dfs(grid:List[List[str]], i, j):
-    # g[i][j] 에서 4방향 탐색을 진행한다. '0'이 나올때 까지
-    if i < 0 or i >= len(grid) or \
-        j < 0 or j >= len(grid[0]) or \
-            grid[i][j] != '1':
-            return
-    
-    print(grid, grid[i][j])
-    print(type(grid[i][j]))
-    grid[i][j].replace('1', '0') # 방문한 곳은 1을 지워서 중복계산을 방지한다.
-    #grid[i][j].replace('1', '0')
-    print(grid[i][j])
-
-    # 4방향 탐색 (재귀)
-    '''
-    dfs(grid, i+1, j)
-    dfs(grid, i-1, j)
-    dfs(grid, i, j+1)
-    dfs(grid, i, j-1)
-    '''
-
-
-input = ['11110',
-'11010',
-'11000',
-'00000']
-
-print(solutions(input))
+    print(numIslands(input))
