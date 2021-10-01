b710d3aeaff6c5a1cc57d59c652431c901eccdfa
Damas J Lee <sophieadenn@gmail.com>
2021-10-01 19:45:01 +0900

    #42. Maximum depth of B-Tree

--- 14 Tree/42. Maximum Depth of Binary Tree.py
+++ 14 Tree/42. Maximum Depth of Binary Tree.py
@@ -6,11 +6,26 @@
 
 import collections
 
-def maxDepth(root):
-    ...
+def maxDepth(root:TreeNode):
+    if root is None:
+        return 0
     queue = collections.deque([root])
     depth = 0
 
     while queue:
-        ...
+        # 깊이 추가
+        depth += 1
+        # 큐 연산 추출 노드를 자식 노드로 삽입
+        for _ in range(len(queue)):
+            cur_root = queue.popleft()
+            # 자식 노드 유무 판별
+            if cur_root.left:
+                queue.append(cur_root.left)
+            if cur_root.right:
+                queue.append(cur_root.right)
+        
+        # BFS의 반복 횟수가 곧 깊이가 된다.
     return depth
+
+root = [3,9,20, None, None, 15, 7 ]
+print(maxDepth(root))
